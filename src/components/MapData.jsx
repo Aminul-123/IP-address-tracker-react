687c4d10f081a2806ac0215ec3929ad7a351d868
Aminul Ali <alomamin963@gmail.com>
2024-09-28 07:26:16 +0530

    error handling in useEffect

--- src/components/MapData.jsx
+++ src/components/MapData.jsx
@@ -7,6 +7,9 @@ function MapData() {
 
     useEffect(function () {
         async function getDataFromIP () {
+            try {
+
+            
             const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY_IP_ADDRESS}&ipAddress=${query}`)
             if (!res.ok) throw new Error ('There was an Error in Checking API Address.');
 
@@ -15,12 +18,16 @@ function MapData() {
            // console.log(data?.location?.lat, data?.location?.lng);
             setInfo(data)
             setDefaultPosition([data?.location?.lat, data?.location?.lng]);
+            }
+            catch(error) {
+                console.log(error);
+            }
         }
 
       getDataFromIP();
     }, [query])
     //there will be IP Address in the above dependency, we will take the IP Adress from input field which is in headers, if needed use context api.
-    // next is integrate map api with api key, and set this lat and lng to that api url. 
+     
   return (
     <div className="mapData">
         <div>
